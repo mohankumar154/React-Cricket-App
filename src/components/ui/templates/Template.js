fe8be88918e65255f3226be67fafe416f48f7873
Mohankumar d <Mohankumar.d@borngroup.com>
2020-07-09 01:00:36 +0530

    api data issue fixed

--- src/components/ui/templates/Template.js
+++ src/components/ui/templates/Template.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../header/Header';
 import Routes from '../routes/Routes';
+import Home from '../home/Home';
 
 const Template = () => (
   <div className="container">
     <Header />
+    <Home/>
     <Routes />
   </div>
 );
