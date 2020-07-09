a3ed3df7e81959a7a4538937bafdc685429cbd15
Mohankumar d <Mohankumar.d@borngroup.com>
2020-07-09 08:29:23 +0530

    api data issue fixed

--- src/components/ui/templates/Template.js
+++ src/components/ui/templates/Template.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Header from '../header/Header';
 import Routes from '../routes/Routes';
+import Home from '../home/Home';
 
 
 const Template = () => (
   
   <div className="container">
-    {console.log("mohanqq",window.location.pathname)}
     <Header />
     <Routes />
-    
-  
+    {window.location.pathname=="/React-Cricket-App/" || window.location.pathname=="/React-Cricket-App"?
+    <Home />
+    :""}
   </div>
   
  
